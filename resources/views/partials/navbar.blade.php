4ac025687a9d37dd763b7a8c60e29d038159384a
Yobato <satriyobagus30077@gmail.com>
2021-11-12 15:51:17 +0700

    Front End - Homepage + Navbar

---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,16 @@
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand fw-bold" style="color: #C5AC58; font-family: 'Roboto';" href="#">Tembaga</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+        <div class="navbar-nav"> 
+          <a class="nav-link active" aria-current="page" href="#">Beranda</a>
+          <a class="nav-link" href="#">Layanan</a>
+          <a class="nav-link" href="#">Sewa</a>
+          <a class="nav-link" href="#" tabindex="-1"><img class = "img-fluid" style="height: 30px; " src="img/profile.png"></a>
+        </div>
+      </div>
+    </div>
+  </nav>
